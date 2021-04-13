e578789e279fca9f5aa308062b1014e89badb72a
Saleek <saleek@ontash.net>
2021-04-13 18:07:42 +0530

    Create Task

--- task/urls.py
+++ task/urls.py
@@ -7,9 +7,10 @@ from django.urls import path
 #    path('', views.tasklist, name = 'task')]
 
 #Routes for Class Based View
-from . views import TaskList, TaskDetail
+from . views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
     path('', TaskList.as_view(), name = 'tasks'),
-    path('task/<int:pk>',TaskDetail.as_view(), name = 'task')
+    path('task/<int:pk>',TaskDetail.as_view(), name = 'task'),
+    path('task-create/',TaskCreate.as_view(), name = 'task-create')
 ]
--- task/views.py
+++ task/views.py
@@ -4,8 +4,15 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 
 #For Class Based views
+
+#For listing all tasks
 from django.views.generic.list import ListView
+#For listing a Specific Task
 from django.views.generic.detail import DetailView
+#For Creating a Task
+from django.views.generic.edit import CreateView
+#For reverse_url
+from django.urls import reverse_lazy
 
 #importing model class
 from .models import Task
@@ -17,12 +24,22 @@ from .models import Task
 #    return HttpResponse('To Do List')
 
 #Class based views
+
+#Class for listing all tasks
 class TaskList(ListView):
     model = Task
     context_object_name = 'Tasks'
     template_name = 'task/task_list.html'
 
+#Class for listing specific task
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'Task'
     template_name = 'task/task.html'
+
+#Class for creating a task
+class TaskCreate(CreateView):
+    model = Task
+    #fileds = ['title','description']
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
